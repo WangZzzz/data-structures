bd208a16f0072372dfefde9eff3a543df4852409
WangZzzz <wztc_001@bupt.edu.cn>
2015-05-11 23:46:14 +0800

    201505112346

--- src/Sort/InsertionSortDemo.java
+++ src/Sort/InsertionSortDemo.java
@@ -1,5 +1,8 @@
 package Sort;
 
+/*
+ * 直接插入排序：将左边的视为已经排好序的，将右边的依次插入
+ */
 public class InsertionSortDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,5 +18,19 @@ public class InsertionSortDemo {
 			return;
 		}
 		int n = data.length;
+		int i,j,k,tmp;;
+		for(i = 1; i< n; i++){
+			//寻找插入位置
+			for(j = 0; j <= i - 1; j++){
+				if(data[j] > data[i])
+					break;
+			}
+			tmp = data[i];
+			//依次向后移动，将位置j留给data[i]这个值
+			for(k = i - 1; k >= j; k--){
+				data[k+1] = data[k];
+			}
+			data[j] = tmp;
+		}
 	}
 }
