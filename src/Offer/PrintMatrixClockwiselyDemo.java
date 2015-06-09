2751e1a206a71010ae832e43f009513423e42a34
WangZzzz <wztc_001@bupt.edu.cn>
2015-06-09 22:31:02 +0800

    201506092230

--- src/Offer/PrintMatrixClockwiselyDemo.java
+++ src/Offer/PrintMatrixClockwiselyDemo.java
@@ -4,7 +4,11 @@ public class PrintMatrixClockwiselyDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+	}
+	
+	public static void PrintMatrixClockwisely(int[][] nums){
+		
 	}
 
 }
